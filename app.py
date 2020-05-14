61eefdf1b1cf973b0ac2bdc618650789974b9865
Marshall Zhao <zhaomarshall@gmail.com>
2020-05-13 22:31:12 -0700

    upload simulator

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ import altair as alt
 import seaborn as sns; sns.set()
 from helpers import *
 from SIR_Model import *
+from scipy.integrate import odeint
+
+
 
 
 
 def main():
     st.sidebar.title("Navigation")
     page = st.sidebar.radio("Go to",
-                             ('Data Exploratory', 'Forecast Model'))
+                             ('Data Exploratory', 'Forecast Model','SIR Simulation'))
     if page == 'Data Exploratory':
         st.title('Explore County Level Data ')
         # load data
@@ -38,6 +41,48 @@ def main():
         d = base.encode(y='New deaths', color=red).properties(title='Daily New Deaths')
         st.altair_chart(d,use_container_width=True)
 
+    elif page == 'SIR Simulation':
+        st.title('SIR Simulation')
+        st.subheader('This is toy for SIR model with customized parameters')
+        N = st.slider('Input the populaiton', 100000,10000000, step = 100000,value = 3000000)
+        I0 = st.slider('Input initial infection',1,5000,step = 5,value = 200)
+        R0 = st.slider('Input initial removed',0,1000,step = 1,value = 0)
+        beta = st.number_input('Input beta', min_value=0.0, max_value=10.0,value = 0.2)
+        gamma = st.number_input('Input gamma',min_value = 0.0, max_value = 1.0, value = 0.08)
+
+        # Everyone else, S0, is susceptible to infection initially.
+        S0 = N - I0 - R0
+        # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
+        # A grid of time points (in days)
+        t = np.linspace(0, 200, 500)
+
+        # The SIR model differential equations.
+        def deriv(y, t, N, beta, gamma):
+            S, I, R = y
+            dSdt = -beta * S * I / N
+            dIdt = beta * S * I / N - gamma * I
+            dRdt = gamma * I
+            return dSdt, dIdt, dRdt
+
+        # Initial conditions vector
+        y0 = S0, I0, R0
+        # Integrate the SIR equations over the time grid, t.
+        ret = odeint(deriv, y0, t, args=(N, beta, gamma))
+        S, I, R = ret.T
+
+        # Plot the data on three separate curves for S(t), I(t) and R(t)
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.plot(t, S / N, 'b', lw=2, label='Susceptible')
+        ax.plot(t, I / N, 'r', lw=2, label='Infected')
+        ax.plot(t, R / N, 'g', lw=2, label='Recovered with immunity')
+        ax.set_xlabel('Time /days')
+        ax.set_ylabel('Number (1000s)')
+        ax.set_ylim(0, 1.2)
+        legend = ax.legend()
+        for spine in ('top', 'right', 'bottom', 'left'):
+            ax.spines[spine].set_visible(False)
+        plt.show()
+        st.pyplot()
 
     else:
         st.title('County Level Covid-19 Forecast Model')
